47b21167379689ca221911388c7a69c73f5f91a7
kangenboci <60374629+kangenboci@users.noreply.github.com>
2024-03-20 11:30:10 +0700

    Update run.sh
    Update lava rpc

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-acc55c37-5180-40c1-be83-977680e46fcd/)
 
 
 echo "$curl_output" >> Update.tsx
